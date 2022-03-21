aa6c8732fce81023e394d228a72fc8919c99dce5
gwanduke <gwanduke@gmail.com>
2022-03-22 01:01:10 +0900

    ~ 2022-03-22 : Long Polling

--- try/realtime-web/server.js
+++ try/realtime-web/server.js
@@ -1,11 +1,76 @@
 const http = require("http");
 var fs = require("fs");
+let url = require("url");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 const clients = new Set();
 
+let subscribers = Object.create(null);
+
+let longPollingCount = 0;
+
+function onSubscribe(req, res) {
+  let id = Math.random();
+
+  res.setHeader("Content-Type", "text/plain;charset=utf-8");
+  res.setHeader("Cache-Control", "no-cache, must-revalidate");
+
+  subscribers[id] = res;
+
+  req.on("close", function () {
+    delete subscribers[id];
+  });
+}
+
+function publish(message) {
+  for (let id in subscribers) {
+    let res = subscribers[id];
+    res.end(message);
+  }
+
+  subscribers = Object.create(null);
+}
+
 const server = http.createServer((req, res) => {
+  let urlParsed = url.parse(req.url, true);
+  if (urlParsed.pathname == "/subscribe") {
+    onSubscribe(req, res);
+
+    if (longPollingCount < 5) {
+      setTimeout(() => {
+        longPollingCount++;
+        req.setEncoding("utf8");
+        let message = "asdfads";
+        req
+          .on("data", function (chunk) {
+            message += chunk;
+          })
+          .on("end", function () {
+            publish(message); // publish it to everyone
+          });
+      }, 1000);
+    }
+    return;
+  }
+
+  // sending a message
+  if (urlParsed.pathname == "/publish" && req.method == "POST") {
+    // accept POST
+    req.setEncoding("utf8");
+    let message = "";
+    req
+      .on("data", function (chunk) {
+        message += chunk;
+      })
+      .on("end", function () {
+        publish(message); // publish it to everyone
+        res.end("ok");
+      });
+
+    return;
+  }
+
   fs.readFile("./index.html", "utf-8", (err, data) => {
     res.statusCode = 200;
     res.setHeader("Content-Type", "text/html");
@@ -45,6 +110,23 @@ webSocketServer.on("connection", (webSocket, request) => {
   });
 });
 
+function close() {
+  for (let id in subscribers) {
+    let res = subscribers[id];
+    res.end();
+  }
+}
+
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
 });
+
+if (process.send) {
+  process.on("message", (msg) => {
+    if (msg === "shutdown") {
+      close();
+    }
+  });
+}
+
+process.on("SIGINT", close);
